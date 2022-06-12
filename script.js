83312994198d9913b9d0249ad3757e492f4f9afe
Ana Carvalho <anapaulacmoura@gmail.com>
2022-06-12 20:57:27 -0300

    Adding confirm button js

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
+
 function selecionadoComida(card){
-    let selecionar = document.querySelector(".card-comida-selecionado");
-    if (selecionar !== null) {
-        selecionar.classList.remove("card-comida-selecionado");
+     let selecionarComida = document.querySelector(".card-comida-selecionado");
+
+        if (selecionarComida !== null) {
+        selecionarComida.classList.remove("card-comida-selecionado");
 
     }
+
 card.classList.add("card-comida-selecionado");
+
+const comida = document.querySelector('.card-comida-selecionado');
+const bebida = document.querySelector('.card-bebida-selecionado');
+const sobremesa = document.querySelector('.card-sobremesa-selecionado');
+const botao = document.querySelector('.botao');
+
+if ((comida !== null) && (bebida !== null) && (sobremesa !== null)) {
+    botao.innerHTML = 'Fechar pedido';
+    botao.classList.add('botao-selecionado');
+}
 }
 
 function selecionadoBebida(card){
-    let selecionar = document.querySelector(".card-bebida-selecionado");
-    if (selecionar !== null) {
-        selecionar.classList.remove("card-bebida-selecionado");
+    let selecionarBebida = document.querySelector(".card-bebida-selecionado");
+    if (selecionarBebida !== null) {
+        selecionarBebida.classList.remove("card-bebida-selecionado");
 
     }
 card.classList.add("card-bebida-selecionado");
+
+const comida = document.querySelector('.card-comida-selecionado');
+const bebida = document.querySelector('.card-bebida-selecionado');
+const sobremesa = document.querySelector('.card-sobremesa-selecionado');
+const botao = document.querySelector('.botao');
+
+if ((comida !== null) && (bebida !== null) && (sobremesa !== null)) {
+    botao.innerHTML = 'Fechar pedido';
+    botao.classList.add('botao-selecionado');
+}
 }
 
 function selecionadoSobremesa(card){
-    let selecionar = document.querySelector(".card-sobremesa-selecionado");
-    if (selecionar !== null) {
-        selecionar.classList.remove("card-sobremesa-selecionado");
+    let selecionarSobremesa = document.querySelector(".card-sobremesa-selecionado");
+    if (selecionarSobremesa !== null) {
+        selecionarSobremesa.classList.remove("card-sobremesa-selecionado");
 
     }
 card.classList.add("card-sobremesa-selecionado");
+
+const comida = document.querySelector('.card-comida-selecionado');
+const bebida = document.querySelector('.card-bebida-selecionado');
+const sobremesa = document.querySelector('.card-sobremesa-selecionado');
+const botao = document.querySelector('.botao');
+
+if ((comida !== null) && (bebida !== null) && (sobremesa !== null)) {
+    botao.innerHTML = 'Fechar pedido';
+    botao.classList.add('botao-selecionado');
 }
+}
+
+
+
+
+
+
+
+
